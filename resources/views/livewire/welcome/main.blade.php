e0a8c93aeea3b241731c61eadb1699130836336f
Matt Strauss <strausstester@gmail.com>
2024-12-22 15:16:14 -0500

    WIP

--- resources/views/livewire/welcome/main.blade.php
+++ resources/views/livewire/welcome/main.blade.php
@@ -3,7 +3,7 @@
         <div class="flex flex-col items-center justify-center py-10 lg:py-20">
             <h1 class="text-4xl font-bold text-center text-[#FF2D20] lg:text-5xl">Gym</h1>
             <p class="mt-4 text-center text-lg text-gray-600 lg:text-xl">
-                Simple, fast, reliable workout tracker.
+                Simple, fast, reliable workout tracker
             </p>
             <div class="mt-8">
                 <a href="{{ route('register') }}" class="inline-block px-6 py-3 text-lg font-semibold text-white bg-[#FF2D20] rounded-lg hover:bg-[#FF1A00]">Get Started</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,16 +17,16 @@
 
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                    @if (Route::has('login'))
+                        <livewire:welcome.navigation />
+                    @endif
+                    <header class="grid grid-cols-2 items-center gap-2 pt-10 pb-2 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
                             <x-heroicon-o-cube-transparent/>
                         </div>
-                        @if (Route::has('login'))
-                            <livewire:welcome.navigation />
-                        @endif
                     </header>
 
-                    <main class="mt-6">
+                    <main class="mt-1">
                         @include('livewire.welcome.main')
                     </main>
                 </div>
