a37486f035b5f9271566c748b2d840789ce51fd2
Matt Strauss <strausstester@gmail.com>
2024-12-22 12:35:29 -0500

    Initial commit

---
+++ resources/views/livewire/welcome/main.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <div>
+        <div class="flex flex-col items-center justify-center py-10 lg:py-20">
+            <h1 class="text-4xl font-bold text-center text-[#FF2D20] lg:text-5xl">Gym</h1>
+            <p class="mt-4 text-center text-lg text-gray-600 lg:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div class="mt-8">
+                <a href="{{ route('register') }}" class="inline-block px-6 py-3 text-lg font-semibold text-white bg-[#FF2D20] rounded-lg hover:bg-[#FF1A00]">Get Started</a>
+            </div>
+        </div>
+        <div class="flex items
+        -center justify-center">
+            <i class="fas fa-dumbbell"></i>
+        </div>
+    </div>
+
+</div>
