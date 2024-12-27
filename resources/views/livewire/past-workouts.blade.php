07739a7050a5561955664777b46f3f40a3259330
Matt Strauss <strausstester@gmail.com>
2024-12-27 09:08:10 -0500

    Update past-workouts.blade.php

--- resources/views/livewire/past-workouts.blade.php
+++ resources/views/livewire/past-workouts.blade.php
@@ -1,10 +1,10 @@
 <div>
-    <div class="flex justify-between items-center">
+    <div class="flex justify-between items-center mb-3">
         <h1 class="text-2xl font-bold">Past Workouts</h1>
-        <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">New Workout</a>
+        <a href="#" class="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">New Workout</a>
     </div>
     <div class="mt-4">
-        <table class="w-full">
+        <table class="w-full table-auto border-separate border-spacing-y-5">
             <thead>
                 <tr>
                     <th class="text-left">Name</th>
@@ -12,7 +12,7 @@
                     <th class="text-left">Duration</th>
                     <th class="text-left"># Reps</th>
                     <th class="text-left">Total Weight</th>
-                    <th class="text-left">Actions</th>
+                    <th class="text-left">View</th>
                 </tr>
             </thead>
             <tbody>
@@ -23,9 +23,10 @@
                         <td>{{ $workout->duration }}</td>
                         <td>{{ $workout->reps }}</td>
                         <td>{{ $workout->total_weight }}</td>
-                        <td>
-                            <a href="#" class="text-blue-500">Edit</a>
-                            <a href="#" wire:click.prevent="delete({{ $workout->id }})" class="text-red-500 ml-2">Delete</a>
+                        <td class="flex">
+                            <a href="#">
+                                <x-heroicon-o-arrow-right-end-on-rectangle class="cursor-pointer ml-2 w-8 h-8" />
+                            </a>
                         </td>
                     </tr>
                 @endforeach
