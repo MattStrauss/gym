f667df379410650556d6b90ec91d120c668fa2da
Matt Strauss <strausstester@gmail.com>
2024-12-29 10:58:02 -0500

    WIP

--- app/Livewire/ViewWorkout.php
+++ app/Livewire/ViewWorkout.php
@@ -3,18 +3,38 @@
 namespace App\Livewire;
 
 use App\Models\Workout;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class ViewWorkout extends Component
 {
     public Workout $workout;
+    public Collection $exercises;
     public string $title = '';
 
     public function mount(Workout $workout): void
     {
-        $this->workout = $workout->load('exercises');
+        $this->workout = $workout;
+        $this->exercises = $this->organizeWorkoutIntoSets();
         $this->title = $workout->name;
+    }
+
+    private function organizeWorkoutIntoSets(): Collection
+    {
+        return $this->workout->exercises->groupBy('name')->map(function ($exercise) {
+            $sets = [];
+            foreach ($exercise as $set) {
+                $sets[] = [
+                    'reps' => $set->pivot->reps,
+                    'weight' => $set->pivot->weight,
+                    'is_drop_set' => $set->pivot->is_drop_set,
+                    'super_set_number' => $set->pivot->super_set_number,
+                ];
+            }
+
+            return $sets;
+        });
     }
 
     public function render(): View
--- resources/views/livewire/view-workout.blade.php
+++ resources/views/livewire/view-workout.blade.php
@@ -11,19 +11,25 @@
                         <h4 class="font-semibold text-lg text-gray-800 dark:text-gray-200 leading-tight">
                             Exercises
                         </h4>
-                        <ul class="mt-2">
-                            @foreach ($workout->exercises as $exercise)
-                                <li class="flex justify-between items-center py-2">
-                                    <div>
-                                        <h5 class="font-semibold text-md text-gray-800 dark:text-gray-200 leading-tight">
-                                            {{ $exercise->name }}
-                                        </h5>
+                        <ul class="mt-2 grid grid-cols-4 gap-4">
+                            @foreach ($exercises as $exerciseName => $sets)
+                                <li class="py-2">
+                                    <div class="w-full">
+                                        <h3 class="font-semibold text-md text-gray-800 dark:text-gray-200 leading-tight">
+                                            {{ $exerciseName }}
+                                        </h3>
                                     </div>
-                                    <div>
-                                        <p class="text-gray-600 dark:text-gray-400">
-                                            {{ $exercise->pivot->reps }} reps @ {{ $exercise->pivot->weight }}
-                                        </p>
-                                    </div>
+                                    @foreach($sets as $set)
+                                        <ul class="">
+                                            <li class="flex flex-row items center py-2">
+                                                <div class="">
+                                                    <p class="text-gray-600 dark:text-gray-400">
+                                                        {{ $set['reps'] }} reps @ {{ $set['weight'] }}
+                                                    </p>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    @endforeach
                                 </li>
                             @endforeach
                         </ul>
